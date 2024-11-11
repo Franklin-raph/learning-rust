f8d0f0ddf074c29565d9b1f57cb32c3cbbc7bcef
franklin-raph <igboekwulusifranklin@gmail.com>
2024-11-11 12:50:39 +0100

    learnt how to use the standard input and output

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,15 +1,22 @@
+use std::io;
+
 fn main() {
-    const MY_NAME_HERE: u32 = 100;
+    const MY_NAME_HERE: &str = "Frank";
     println!("My name is {}", MY_NAME_HERE);
 
-    let mut x= 6;
+    let mut x= 6; //any un-assingned interger, takes the 32bit signed integer value by default
     println!("X is {}", x);
 
     {
-        let x = 20 - 5;
+        let x: u64 = 20 - 5;
         println!("inside block: {}", x);
     }
 
     x = 8;
     println!("X is now {}", x);
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Faild to real line");
+    println!("{}", input);
+
 }
